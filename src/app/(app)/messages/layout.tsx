18a00fd33161509241e03a01e43f5fdd4df82700
Anindya Kumar Patro <anindya@anindya-mac-4.local>
2026-06-27 13:11:21 +0530

    fix chat height with exact calc

--- src/app/(app)/messages/layout.tsx
+++ src/app/(app)/messages/layout.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 export default function MessagesLayout({ children }) {
   return (
-    <div className="h-full overflow-hidden -mx-4 -my-4 lg:-mx-7 lg:-my-7 mt-0">
+    <div style={{ height: "calc(100vh - 56px)" }} className="overflow-hidden -mx-4 lg:-mx-7 -my-4 lg:-my-7">
       {children}
     </div>
   );
