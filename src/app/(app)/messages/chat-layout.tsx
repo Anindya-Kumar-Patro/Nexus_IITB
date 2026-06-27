fe8cbe08df933475b6e51a856c1605cf5df82935
Anindya Kumar Patro <anindya@anindya-mac-4.local>
2026-06-27 12:51:45 +0530

    fix chat height - dedicated messages layout with no padding

--- src/app/(app)/messages/chat-layout.tsx
+++ src/app/(app)/messages/chat-layout.tsx
@@ -249,7 +249,7 @@ export function ChatLayout({
   );
 
   return (
-    <div className="flex -mx-4 -my-4 lg:-mx-7 lg:-my-7" style={{ height: "calc(100vh - 56px)" }} id="chat-root">
+    <div className="flex h-full" id="chat-root">
 
       {/* Mobile profile full screen */}
       {mobileView === "profile" && selectedApp && (
---
+++ src/app/(app)/messages/layout.tsx
@@ -0,0 +1,8 @@
+// @ts-nocheck
+export default function MessagesLayout({ children }) {
+  return (
+    <div className="h-full overflow-hidden -mx-4 -my-4 lg:-mx-7 lg:-my-7 mt-0">
+      {children}
+    </div>
+  );
+}
